3c2e769da751a4a58a9431507263a6ed61d9c3f3
Technorabilia <simon@technorabilia.com>
2021-04-12 12:53:15 +0200

    Initial commit

---
+++ generate_scripts.py
@@ -0,0 +1,102 @@
+import os
+import yaml
+import requests
+from jinja2 import Environment, FileSystemLoader
+import textwrap
+
+
+def get_project_vars(project_name):
+    project_vars = init_vars.copy()
+
+    vars_url = "https://raw.githubusercontent.com/linuxserver/docker-{}/master/readme-vars.yml".format(
+        project_name)
+    response = requests.get(vars_url)
+    project_vars.update(yaml.load(response.text, Loader=yaml.FullLoader))
+
+    # overrides
+    project_vars["param_container_name"] = project_vars["param_container_name"].replace(
+        "{{ project_name }}", project_vars["project_name"])
+
+    project_vars["project_blurb"] = project_vars["project_blurb"].replace(
+        "[{{ project_name|capitalize }}]({{ project_url }})", project_vars["project_name"].capitalize())
+
+    lines = textwrap.wrap(
+        project_vars["project_blurb"], 78, break_long_words=False)
+    project_vars["project_blurb"] = ""
+    for line in lines:
+        project_vars["project_blurb"] += "# {}\n".format(line)
+
+    for row in project_vars["common_param_env_vars"]:
+        if row["env_var"] == "PGID":
+            row["env_value"] = "${PGID:-100}"
+            row["desc"] = "for UserID"
+        if row["env_var"] == "PUID":
+            row["env_value"] = "${PUID:-1024}"
+            row["desc"] = "for GroupID"
+
+    for row in project_vars["param_env_vars"]:
+        if row["env_var"] == "TZ":
+            row["env_value"] = "Europe/Amsterdam"
+            row["desc"] = "Specify a timezone to use for example Europe/Amsterdam"
+
+    if project_vars["project_logo"] != "" and requests.get(project_vars["project_logo"]).status_code != 200:
+        project_vars["project_logo"] = ""
+
+    if "full_custom_readme" in project_vars.keys() and project_vars["full_custom_readme"] != "":
+        project_vars["project_blurb"] = "This container needs special attention. Please check https://hub.docker.com/r/linuxserver/{} for details.".format(
+            project_vars["project_name"])
+
+    return project_vars
+
+
+vars_url = "https://raw.githubusercontent.com/linuxserver/docker-jenkins-builder/master/vars/common"
+resp = requests.get(vars_url)
+init_vars = yaml.load(resp.text, Loader=yaml.FullLoader)
+
+vars_url = "https://raw.githubusercontent.com/linuxserver/docker-jenkins-builder/master/vars/_container-vars-blank"
+resp = requests.get(vars_url)
+init_vars.update(yaml.load(resp.text, Loader=yaml.FullLoader))
+
+env = Environment(loader=FileSystemLoader(
+    "templates"), trim_blocks=True, lstrip_blocks=True, keep_trailing_newline=True)  # ktn added
+env.globals.update(get_project_vars=get_project_vars)
+
+image_url = "https://fleet.linuxserver.io/api/v1/images"
+response = requests.get(image_url)
+response_json = response.json()
+project_list = response_json["data"]["repositories"]["linuxserver"]
+
+# project_list = [
+#     {"name": "sonarr"}
+# ]
+
+projects = {
+    "projects": project_list
+}
+
+out_basedir = "./lsio"
+os.makedirs(out_basedir, exist_ok=True)
+with open("{}/docker-env.cfg".format(out_basedir), "w") as out_file:
+    out_file.write('''#BASEDIR=/volume1/docker
+#PUID=1024
+#PGID=100
+''')
+
+for project in project_list:
+    print(project["name"])
+    out_dir = "{}/{}".format(out_basedir, project["name"])
+    os.makedirs(out_dir, exist_ok=True)
+
+    project_vars = get_project_vars(project["name"])
+
+    template = env.get_template("docker-run.j2")
+    with open("{}/docker-run.sh".format(out_dir), "w") as out_file:
+        out_file.write(template.render(project_vars=project_vars))
+
+    template = env.get_template("docker-compose.j2")
+    with open("{}/docker-compose.yaml".format(out_dir), "w") as out_file:
+        out_file.write(template.render(project_vars=project_vars))
+
+    template = env.get_template("run-once.j2")
+    with open("{}/run-once.sh".format(out_dir), "w") as out_file:
+        out_file.write(template.render(project_vars=project_vars))
---
+++ generate_templates.py
@@ -0,0 +1,80 @@
+import os
+import json
+import yaml
+import requests
+from jinja2 import Environment, FileSystemLoader
+
+
+def get_project_vars(project_name):
+    project_vars = init_vars.copy()
+
+    vars_url = "https://raw.githubusercontent.com/linuxserver/docker-{}/master/readme-vars.yml".format(
+        project_name)
+    response = requests.get(vars_url)
+    project_vars.update(yaml.load(response.text, Loader=yaml.FullLoader))
+
+    # overrides
+    project_vars["project_blurb"] = project_vars["project_blurb"].replace(
+        "[{{ project_name|capitalize }}]({{ project_url }})", project_vars["project_name"].capitalize())
+
+    for row in project_vars["common_param_env_vars"]:
+        if row["env_var"] == "PGID":
+            row["env_value"] = 100
+            row["desc"] = "for UserID"
+        if row["env_var"] == "PUID":
+            row["env_value"] = 1024
+            row["desc"] = "for GroupID"
+
+    for row in project_vars["param_env_vars"]:
+        if row["env_var"] == "TZ":
+            row["env_value"] = "Europe/Amsterdam"
+            row["desc"] = "Specify a timezone to use for example Europe/Amsterdam"
+
+    if project_vars["project_logo"] != "" and requests.get(project_vars["project_logo"]).status_code != 200:
+        project_vars["project_logo"] = ""
+
+    if "full_custom_readme" in project_vars.keys() and project_vars["full_custom_readme"] != "":
+        project_vars["project_blurb"] = "This container needs special attention. Please check https://hub.docker.com/r/linuxserver/{} for details.".format(
+            project_vars["project_name"])
+
+    return project_vars
+
+
+vars_url = "https://raw.githubusercontent.com/linuxserver/docker-jenkins-builder/master/vars/common"
+resp = requests.get(vars_url)
+init_vars = yaml.load(resp.text, Loader=yaml.FullLoader)
+
+vars_url = "https://raw.githubusercontent.com/linuxserver/docker-jenkins-builder/master/vars/_container-vars-blank"
+resp = requests.get(vars_url)
+init_vars.update(yaml.load(resp.text, Loader=yaml.FullLoader))
+
+env = Environment(loader=FileSystemLoader(
+    "templates"), trim_blocks=True, lstrip_blocks=True)
+env.globals.update(get_project_vars=get_project_vars)
+template = env.get_template("templates-2.0.j2")
+
+image_url = "https://fleet.linuxserver.io/api/v1/images"
+response = requests.get(image_url)
+response_json = response.json()
+project_list = response_json["data"]["repositories"]["linuxserver"]
+
+projects = {
+    "projects": project_list
+}
+
+# projects = {
+#     "projects": [
+#         {"name": "sonarr"},
+#     ]}
+
+out_filename = "templates-2.0.json"
+with open(out_filename, "w") as out_file:
+    out_file.write(template.render(projects))
+
+# check valid json
+with open(out_filename) as in_file:
+    json.load(in_file)
+
+# check filesize
+if os.path.getsize(out_filename) < 200000:
+    raise Exception
