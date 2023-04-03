540b78317554f331bd6a7a7055e1215adf62d632
technorabilia <simon@technorabilia.com>
2023-04-03 18:34:32 +0200

    Refactor generate code

---
+++ common.py
@@ -0,0 +1,119 @@
+# Copyright (C) 2021  Technorabilia
+# Written by Simon de Kraa <simon@technorabilia.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import textwrap
+
+import requests
+import yaml
+
+
+def get_project_vars(project_name, init_vars, mode):
+    project_vars = init_vars.copy()
+
+    vars_url = "https://raw.githubusercontent.com/linuxserver/docker-{}/master/readme-vars.yml".format(
+        project_name)
+    response = requests.get(vars_url)
+    project_vars.update(yaml.load(response.text, Loader=yaml.FullLoader))
+
+    # overrides
+    if mode == "scripts":
+        project_vars["param_container_name"] = project_vars["param_container_name"].replace(
+            "{{ project_name }}", project_vars["project_name"])
+
+    project_vars["project_blurb"] = project_vars["project_blurb"].replace(
+        "{{ project_name|capitalize }}", project_vars["project_name"].capitalize())
+    project_vars["project_blurb"] = project_vars["project_blurb"].replace(
+        "{{ project_name }}", project_vars["project_name"].capitalize())
+    project_vars["project_blurb"] = project_vars["project_blurb"].replace(
+        "{{ project_url }}", project_vars["project_url"])
+    project_vars["project_blurb"] = project_vars["project_blurb"].replace(
+        "\n", " ")
+    project_vars["project_blurb"] = project_vars["project_blurb"].replace(
+        '"', "'")
+    project_vars["project_blurb"] = ' '.join(
+        project_vars["project_blurb"].split())
+
+    if mode == "scripts":
+        lines = textwrap.wrap(
+            project_vars["project_blurb"], 78, break_long_words=False)
+        project_vars["project_blurb"] = ""
+        for line in lines:
+            project_vars["project_blurb"] += "# {}\n".format(line)
+
+    if mode == "scripts":
+        for row in project_vars["common_param_env_vars"]:
+            if row["env_var"] == "PGID":
+                row["env_value"] = "${PGID:-100}"
+                row["desc"] = "for GroupID"
+            if row["env_var"] == "PUID":
+                row["env_value"] = "${PUID:-1024}"
+                row["desc"] = "for UserID"
+            if row["env_var"] == "TZ":
+                row["env_value"] = "${TZ:-Europe/Amsterdam}"
+
+    elif mode == "templates":
+        for row in project_vars["common_param_env_vars"]:
+            if row["env_var"] == "PGID":
+                row["env_value"] = 100
+            if row["env_var"] == "PUID":
+                row["env_value"] = 1024
+            if row["env_var"] == "TZ":
+                row["env_value"] = "Europe/Amsterdam"
+    else:
+        raise ValueError("Invalid mode specified")
+
+    for row in project_vars["param_env_vars"]:
+        if row["env_var"] == "TZ":
+            project_vars["param_env_vars"].remove(row)
+    if len(project_vars["param_env_vars"]) == 0:
+        project_vars["param_usage_include_env"] = False
+
+    if project_vars["project_logo"] == "http://www.logo.com/logo.png":
+        project_vars["project_logo"] = ""
+
+    if "full_custom_readme" in project_vars.keys() and project_vars["full_custom_readme"] != "":
+        project_vars["project_blurb"] = "This container needs special attention. Please check https://hub.docker.com/r/linuxserver/{} for details.".format(
+            project_vars["project_name"])
+
+    return project_vars
+
+
+def get_initial_variables():
+    vars_url = "https://raw.githubusercontent.com/linuxserver/docker-jenkins-builder/master/vars/common"
+    resp = requests.get(vars_url)
+    init_vars = yaml.load(resp.text, Loader=yaml.FullLoader)
+
+    vars_url = "https://raw.githubusercontent.com/linuxserver/docker-jenkins-builder/master/vars/_container-vars-blank"
+    resp = requests.get(vars_url)
+    init_vars.update(yaml.load(resp.text, Loader=yaml.FullLoader))
+
+    return init_vars
+
+
+def get_project_list():
+    image_url = "https://fleet.linuxserver.io/api/v1/images"
+    response = requests.get(image_url)
+    response_json = response.json()
+    project_list = response_json["data"]["repositories"]["linuxserver"]
+
+    project_list = list(
+        filter(lambda project: project["deprecated"] == False, project_list))
+
+    # # testing
+    # project_list = list(
+    #     filter(lambda project: project["name"] == "sonarr", project_list))
+
+    return project_list
--- generate_templates.py
+++ generate_templates.py
@@ -18,83 +18,18 @@
 import json
 import os
 
-import requests
-import yaml
 from jinja2 import Environment, FileSystemLoader
 
+import common
 
-def get_project_vars(project_name):
-    project_vars = init_vars.copy()
-
-    vars_url = "https://raw.githubusercontent.com/linuxserver/docker-{}/master/readme-vars.yml".format(
-        project_name)
-    response = requests.get(vars_url)
-    project_vars.update(yaml.load(response.text, Loader=yaml.FullLoader))
-
-    # overrides
-    project_vars["project_blurb"] = project_vars["project_blurb"].replace(
-        "{{ project_name|capitalize }}", project_vars["project_name"].capitalize())
-    project_vars["project_blurb"] = project_vars["project_blurb"].replace(
-        "{{ project_name }}", project_vars["project_name"].capitalize())
-    project_vars["project_blurb"] = project_vars["project_blurb"].replace(
-        "{{ project_url }}", project_vars["project_url"])
-    project_vars["project_blurb"] = project_vars["project_blurb"].replace(
-        "\n", " ")
-    project_vars["project_blurb"] = project_vars["project_blurb"].replace(
-        '"', "'")
-    project_vars["project_blurb"] = ' '.join(
-        project_vars["project_blurb"].split())
-
-    for row in project_vars["common_param_env_vars"]:
-        if row["env_var"] == "PGID":
-            row["env_value"] = 100
-            row["desc"] = "for GroupID"
-        if row["env_var"] == "PUID":
-            row["env_value"] = 1024
-            row["desc"] = "for UserID"
-        if row["env_var"] == "TZ":
-            row["env_value"] = "Europe/Amsterdam"
-
-    for row in project_vars["param_env_vars"]:
-        if row["env_var"] == "TZ":
-            project_vars["param_env_vars"].remove(row)
-    if len(project_vars["param_env_vars"]) == 0:
-        project_vars["param_usage_include_env"] = False
-
-    if project_vars["project_logo"] == "http://www.logo.com/logo.png":
-        project_vars["project_logo"] = ""
-
-    if "full_custom_readme" in project_vars.keys() and project_vars["full_custom_readme"] != "":
-        project_vars["project_blurb"] = "This container needs special attention. Please check https://hub.docker.com/r/linuxserver/{} for details.".format(
-            project_vars["project_name"])
-
-    return project_vars
-
-
-vars_url = "https://raw.githubusercontent.com/linuxserver/docker-jenkins-builder/master/vars/common"
-resp = requests.get(vars_url)
-init_vars = yaml.load(resp.text, Loader=yaml.FullLoader)
-
-vars_url = "https://raw.githubusercontent.com/linuxserver/docker-jenkins-builder/master/vars/_container-vars-blank"
-resp = requests.get(vars_url)
-init_vars.update(yaml.load(resp.text, Loader=yaml.FullLoader))
+init_vars = common.get_initial_variables()
+project_list = common.get_project_list()
 
 env = Environment(loader=FileSystemLoader(
     "templates"), trim_blocks=True, lstrip_blocks=True)
-env.globals.update(get_project_vars=get_project_vars)
+env.globals.update(init_vars=init_vars)
+env.globals.update(get_project_vars=common.get_project_vars)
 template = env.get_template("templates-2.0.j2")
-
-image_url = "https://fleet.linuxserver.io/api/v1/images"
-response = requests.get(image_url)
-response_json = response.json()
-project_list = response_json["data"]["repositories"]["linuxserver"]
-
-# # testing
-# project_list = list(
-#     filter(lambda project: project["name"] == "sonarr", project_list))
-
-project_list = list(
-    filter(lambda project: project["deprecated"] == False, project_list))
 
 projects = {
     "projects": project_list
